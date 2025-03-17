076544cc3a368592c116e44231f3be204269f5d7
ANOON01 <pacholikszymon@protonmail.com>
2025-03-17 19:26:09 +0100

    Pierwszy zapis do repozytorium LAB2

---
+++ src/MojaDrugaAplikacja.java
@@ -0,0 +1,22 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class MojaDrugaAplikacja {
+    public static void main (String[] args) {
+        //Stworzenie zmiennej x i przypisanie wartości 10
+        int x = 10 ;
+
+        //Obliczenie dwukrotności x
+        int dwukrotnoscX = 2 * x;
+
+        //obliczenie wartości x2
+        int xKwadrat = x * x;
+
+        //wyświetlenie wyników na konsoli
+        System.out.println("x = " + x);
+        System.out.println("dwukrotność x = " + dwukrotnoscX);
+        System.out.println("x2 = " + xKwadrat);
+
+    }
+}
--- src/MojaPierwszaAplikacja.java
+++
@@ -1,5 +0,0 @@
-public class MojaPierwszaAplikacja {
-    public static void main (String[] args) {
-        System.out.println("Witaj PP");
-    }
-}
